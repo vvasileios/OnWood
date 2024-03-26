57e248cb4de7a99d0d307885532be93e08e1ddb1
Vasileios <vtvasileiadis@gmail.com>
2024-03-26 20:43:34 +0200

    minor

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
-import "./assets/index.css";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
+import router from "./router/index.js";
+
+import "./assets/index.css";
 
 const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
 
 app.mount("#app");
